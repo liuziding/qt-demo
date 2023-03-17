6ad7af6587cd2e195b461dd15da4ee38ed446b5b
liuziding <liu_ziding@163.com>
2023-03-17 15:43:56 +0800

    submit

--- components/main.py
+++ components/main.py
@@ -1,19 +1,41 @@
 import sys
 
-from PySide6.QtWidgets import (QWidget, QHBoxLayout, QPushButton, QGridLayout, QLabel, QVBoxLayout, QSplitter,
-                               QMainWindow, QApplication)
-from PySide6.QtCore import Signal, QFile
-from PySide6.QtGui import Qt, QPixmap
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
 
-from ui.ui_main import Ui_MainWindow
+from ui.ui_main02 import Ui_MainWindow
 from .subLineView import SubLineView
 from components.subAreaView import SubAreaView
+
+image_w = 200
+image_h = 120
 
 class MainPage(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.show()
+
+        self.ui.l_imageArea.setAlignment(Qt.AlignCenter)
+        self.pixmap = QPixmap("images/inside.jpg")
+        self.pixmap_w = self.pixmap.width()
+        self.pixmap_h = self.pixmap.height()
+        label_w = self.ui.l_imageArea.width() - 10
+        label_h = self.ui.l_imageArea.height() - 10
+        print("1234234")
+        print(label_w)
+        print(label_h)
+        ratio_w = self.pixmap_w / label_w
+        ratio_h = self.pixmap_h / label_h
+        if ratio_w > ratio_h:
+            new_pixmap = self.pixmap.scaled((self.pixmap_w / ratio_w), (self.pixmap_h / ratio_w))
+        else:
+            new_pixmap = self.pixmap.scaled((self.pixmap_w / ratio_h), (self.pixmap_h / ratio_h))
+            
+        self.ui.l_imageArea.setPixmap(new_pixmap)
+
 
     def startClicked(self): # 点击“start”按钮  信号与槽函数
         print("你点击了start按钮")
@@ -39,3 +61,25 @@ class MainPage(QMainWindow):
 
     def setArea_text(self, chose_area): # 画区域后点击确认，将坐标值显示出来
         self.ui.setAreaLlineEdit.setText(chose_area)
+
+    def resizeEvent(self, evt):
+        
+        if self.ui.l_imageArea:
+
+            print(self.ui.l_imageArea.size())
+            label_w = self.ui.l_imageArea.width() - 10
+            label_h = self.ui.l_imageArea.height() - 10
+            print("456457456")
+            print(label_w)
+            print(label_h)
+            ratio_w = self.pixmap_w / label_w
+            ratio_h = self.pixmap_h / label_h
+            if ratio_w > ratio_h:
+                new_pixmap = self.pixmap.scaled((self.pixmap_w / ratio_w), (self.pixmap_h / ratio_w))
+            else:
+                new_pixmap = self.pixmap.scaled((self.pixmap_w / ratio_h), (self.pixmap_h / ratio_h))
+                
+            self.ui.l_imageArea.setPixmap(new_pixmap)
+
+        super().resizeEvent(evt)
+    
